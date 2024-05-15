94e9951250b58f7fd6a3858c37aa2b02d1b4a036
prashantkumbhar2002 <prashantkumbhar5515@gmail.com>
2024-05-15 22:46:59 +0530

    * Added support for the standard response
    * Added support for the standard error
    
    * Added support for the async handler function to ensure all promises resolved successfully
    
    * Added support for the cloudinary service support to upload avatar image

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,46 @@
+import fs from 'fs'
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloudinary = async (localpath) => {
+    try {
+        if (!localpath) return null;
+        // Upload the file to Cloudinary
+        const response = await cloudinary.uploader.upload(localpath, { resource_type: "auto" });
+        // Remove the file from the server
+        fs.unlinkSync(localpath);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localpath); // Remove the file from the server
+        return null;
+    }
+        
+        
+}
+
+const deleteFromCloudinary = async (CloudinaryURL, resource_type= "image") => {
+    // console.log(" ~ deleteFromCloudinary ~ CloudinaryURL:", CloudinaryURL)
+    try {
+
+        const PublicId = CloudinaryURL.split('/').pop().split('.')[0];
+        // console.log(" ~ deleteFromCloudinary ~ PublicId:", PublicId)
+
+        if (!PublicId) return null;
+        // Delete the file from Cloudinary
+        const response = await cloudinary.uploader.destroy(PublicId, {
+            resource_type: `${resource_type}`
+        });
+        // console.log(" ~ deleteFromCloudinary ~ response:", response)
+        return response;
+        
+    } catch (error) {
+        return null;
+    }
+}
+
+export { uploadOnCloudinary, deleteFromCloudinary };
