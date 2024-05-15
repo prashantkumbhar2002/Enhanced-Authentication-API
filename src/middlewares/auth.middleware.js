80492ecef23d2e68773345939b1b7d666eb8bf3b
prashantkumbhar2002 <prashantkumbhar5515@gmail.com>
2024-05-15 22:52:35 +0530

    Added auth middleware to ensure authenticated access to the private resources

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,23 @@
+import { User } from "../models/user.models.js";
+import { APIError } from "../utils/apiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+    try {
+        const accessToken = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","");
+        if(!accessToken){
+            throw new APIError(401, "Unauthorized Request");
+        }
+        const decodedToken = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken");
+        if(!user){
+            throw new APIError(401, "Invalid Access Token")
+        }
+        // console.log("User ============", user)
+        req.user = user;
+        next();
+    } catch (error) {
+        throw new APIError(401, error?.message || "Invalid Access Token");
+    }
+})
